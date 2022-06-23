8ab3417cf9805fa9ca24fd5be69db83304fd3f2f
Unyielding1 <david.verde.alvarez@gmail.com>
2022-06-23 14:40:48 -0500

    API

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,14 @@
 import '../styles/main.scss';
-
+import getData from './getdata';
+import showMovies from './showMovies';
 import toggleHamburgerMenu from './hamburguer.js';
+document.addEventListener('DOMContentLoaded', showMovies);
+
+const displayItem = async () => {
+  const movies = await getData();
+  showMovies(movies);
+};
+displayItem();
 
 const hamburguerBtn = document.getElementById('hamburger-btn');
 hamburguerBtn.addEventListener('click', toggleHamburgerMenu);
