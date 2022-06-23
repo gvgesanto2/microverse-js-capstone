b3acc1cf1eb8e2eb25ddaf1c4ab1e42075f15441
Gabriel Santo <gvgesanto2@gmail.com>
2022-06-23 02:44:25 -0300

    feat: Create the details popup basic structure and generate it dynamically

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,16 @@
+import DetailsPopupView from './views/view-components/DetailsPopupView.js';
+import '../assets/images/sprite.svg';
 import '../styles/main.scss';
+
+const tvShowExampleData = {
+  title: 'Under the Dome',
+  premieredYear: 2011,
+  status: 'Ended',
+  rating: 8.8,
+};
+const eventHandlersObj = {
+  handleClosePopup: () => { console.log('close popup'); },
+};
+
+const detailsPopupView = new DetailsPopupView(tvShowExampleData, eventHandlersObj);
+detailsPopupView.render('root');
---
+++ src/scripts/utils/html.utils.js
@@ -0,0 +1,26 @@
+/* eslint-disable import/prefer-default-export */
+export const createHtmlElement = ({
+  tag, className, id, style, text,
+}) => {
+  const elem = document.createElement(tag);
+  if (className) {
+    elem.className = className;
+  }
+
+  if (id) {
+    elem.id = id;
+  }
+
+  if (style) {
+    Object.keys(style).forEach((propKey) => {
+      elem.style[propKey] = style[propKey];
+    });
+  }
+
+  if (text) {
+    const textNode = document.createTextNode(text);
+    elem.appendChild(textNode);
+  }
+
+  return elem;
+};
